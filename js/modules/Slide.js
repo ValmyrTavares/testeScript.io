d3c223103da1d0c5b219b5cec794e04057b8c89f
Valmyr Tavares <valmyr.lima@millennium.com.br>
2021-03-05 16:10:52 -0300

    Estou com o slide funcionando

---
+++ js/modules/Slide.js
@@ -0,0 +1,26 @@
+export default class Slide{
+    constructor(){
+        this.img = document.querySelectorAll('[data-img] img')
+        this.index = 0
+    }
+    addEvent(){
+        this.changeImg()       
+    }    
+    changeImg(){
+        setInterval(()=>{
+            this.index++;
+            this.img.forEach((item) => {
+                item.classList.add('hide')            
+               
+                
+                if(this.index>3)this.index=0
+            })
+            this.img[this.index].classList.remove('hide') 
+            console.log(this.index)
+        },3000)      
+    }
+
+    init(){
+       this.addEvent()
+    }
+}
--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
+import Slide from './modules/Slide.js'
+
+
+
+const slide = new Slide();
+slide.init()
 console.log("Ta rolando")
